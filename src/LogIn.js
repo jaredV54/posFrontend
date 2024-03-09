39f237915f95fb046ebf2dea928505ba22812cb4
Angel_Jared <angeljareddelacruz48@gmail.com>
2024-03-09 22:25:18 +0800

    Update

--- src/LogIn.js
+++ src/LogIn.js
@@ -109,7 +109,7 @@ function LogIn({ values, setValues, handleLogin  }) {
             </div>
             <i className='bx bx-chevrons-right arrow-right'></i>
           </button>
-          <p className='terms-paragraph'>Welcome to PSYZYGY POS!</p>
+          <p className='terms-paragraph'>Welcome to POS webiste!</p>
         </form>
       </div>
     </div>
